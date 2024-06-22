ec4403e74a94b0cd3ec60fcf271715483807ad6e
Salman-S7 <sammyzed1505@gmail.com>
2024-06-22 14:22:19 +0530

    fixed issues

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,11 +5,11 @@ import { BackgroundGradientAnimation } from "../components/ui/WavvyBg"
 const Home = () => {
   return (
       <BackgroundGradientAnimation>
-          <div className="font-mono sm:flex-row flex flex-col w-full h-[100vh] justify-between items-center px-32 z-100">
-              <div className="w-[46%] flex flex-col h-[30%] gap-5">
-                <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 font-extrabold text-6xl">Vector ML Analytics <span className="rounded-full bg-[#3b82f6] text-[#3b82f6]"></span></p>
+          <div className="font-mono sm:flex-row flex flex-col w-full h-[100vh] gap-16 sm:justify-between justify-center items-center sm:px-32 px-4 pt-12 sm:pt-0 z-100">
+              <div className="flex flex-col sm:gap-5 gap-2 w-[100%] sm:w-auto text-center justify-center items-center">
+                <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20 font-extrabold sm:text-6xl text-4xl">Vector ML Analytics<span className="rounded-full bg-[#3b82f6] h-3 w-3 text-[#3b82f6] inline-block"></span></p>
                 <p className="font-bold text-white">Financial Modeling for Banks and Lenders.</p>
-                  <div className="flex justify-start gap-10 items-center">
+                  <div className="flex justify-start sm:gap-10 gap-3 items-center w-[90%]">
                       <Button>
                           Request Demo
                       </Button>
@@ -18,7 +18,7 @@ const Home = () => {
                    </Button>
                   </div>  
               </div>
-              <div className="w-[50%]">
+              <div className="sm:w-[70%]">
                   <img className="object-cover" src='src/assets/home_screen_logo.avif' alt="" />
               </div>
               
